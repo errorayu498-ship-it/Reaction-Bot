9be8fd5e091c4d73225992f545f2901d741c0b73
errorayu498-ship-it <errorayu498@gmail.com>
2026-03-24 03:32:16 +0500

    Create logger.py fixed

---
+++ utils/logger.py
@@ -0,0 +1,33 @@
+import logging
+from colorlog import ColoredFormatter
+from utils.logger import logger
+
+logger.info("Bot started")
+logger.error("Token invalid")
+
+
+def setup_logger():
+
+    handler = logging.StreamHandler()
+
+    formatter = ColoredFormatter(
+        "%(log_color)s[%(levelname)s] %(message)s",
+        log_colors={
+            'DEBUG': 'cyan',
+            'INFO': 'green',
+            'WARNING': 'yellow',
+            'ERROR': 'red',
+            'CRITICAL': 'bold_red',
+        }
+    )
+
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger("bot")
+    logger.setLevel(logging.INFO)
+    logger.addHandler(handler)
+
+    return logger
+
+
+logger = setup_logger()
